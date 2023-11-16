e0024b35c82693ee5ffa392c8f625908d588ff0f
tiaguinzero <tiago.cps123@gmail.com>
2023-11-16 20:33:25 -0300

    adicionando o Footer

--- Site React/src/index.jsx
+++ Site React/src/index.jsx
@@ -21,7 +21,7 @@ import { createBrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CadastroPage/>
+    <HomePage/>
   </React.StrictMode>
 );
 
--- Site React/src/pages/home/home.jsx
+++ Site React/src/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import HeaderComponent from "../../components/header/header"
 import NavComponent from "../../components/nav/nav"
+import FooterComponent from "../../components/footer/footer"
 
 function HomePage(){
     return (
         <>
         <HeaderComponent/>
         <NavComponent/>
+        <FooterComponent/>
         </>
     )
 }
